ed4624ee32473075fff321f18c2e2d0038d694e6
Ambar Pandey <ambar.ara123@gmail.com>
2020-12-14 00:39:18 +0530

    Change Scrollview to FlatList

---
+++ App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, {key:Math.random().toString(), value: enteredGoal}]);
+  };
+  return (
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add your goal"
+          style={styles.textInputStyle}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+      </View>
+
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemdata) => (
+          <View style={styles.listItem}>
+            <Text>{itemdata.item.value}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 30,
+  },
+  textInputStyle: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
