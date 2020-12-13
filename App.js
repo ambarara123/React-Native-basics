c7bb393b30277f41e4679c7ebc151284e3964c7e
Ambar Pandey <ambar.pandey@mutualmobile.com>
2020-12-13 23:43:42 +0530

    add style and text Input

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+
+ const goalInputHandler = (enteredText)=>{
+   setEnteredGoal(enteredText)
+ }
+const addGoalHandler = () => {
+  console.log(enteredGoal)
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <View
+        style={styles.inputContainer}
+      >
+        <TextInput placeholder="Add your goal" style={styles.textInputStyle} onChangeText = {goalInputHandler}
+        value = {enteredGoal} />
+        <Button title="Add"
+        onPress = {addGoalHandler} />
+      </View>
+
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    padding: 30,
   },
+  textInputStyle: {
+    width: '80%',
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
 });
